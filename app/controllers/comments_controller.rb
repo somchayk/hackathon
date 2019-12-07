f6d3c039f1a3b0e43670d3e477dce40b7c060468
Mason Eyre <mason.deyre@gmail.com>
2019-12-07 11:39:45 -0700

    comment controller
    we also gave up on the friggin' seed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,58 @@
 class CommentsController < ApplicationController
+  before_action :set_movie
+  before_action :set_comment, only: [:show, :update, :edit, :destroy]
+
   def index
+    @comments = @movie.comments
   end
 
   def show
   end
 
   def edit
+    render :new
   end
 
   def new
+    @comment = @movie.comments.new
+    render :new
   end
+
+  def create
+    @comment = @movie.comments.new(comment_params)
+    if @comment.save
+      redirect_to [@movie, @comment]
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    reditect_to movie_comment_path(@movie)
+  end
+
+  def update
+    @comment = @movie.comments.new(comment_params)
+    if @comment.save
+      redirect_to [@movie, @comment]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+  
 end
